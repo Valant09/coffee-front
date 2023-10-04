2a4b7e6f7b1ccba14a92f15c51b358c09128b5b6
Cristian Fernando Becerra Guerrero <cfbg99@hotmail.es>
2023-10-03 21:50:12 -0500

    Form added
    Form login

--- coffee-front/src/app/login/login.component.ts
+++ coffee-front/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements  OnInit {
+  username?: string;
+  password?: string;
+
 
   constructor() { }
   ngOnInit() {
+  }
+  onSubmit() {
+    if (this.username && this.password) {
+      const encryptedUsername = CryptoJS.AES.encrypt(this.username, 'secret key').toString();
+      const encryptedPassword = CryptoJS.AES.encrypt(this.password, 'secret key').toString();
+      console.log(encryptedUsername);
+      console.log(encryptedPassword);
+      // Send encryptedUsername and encryptedPassword to the server using HTTPS
+    }
   }
 }
