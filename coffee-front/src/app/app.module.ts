a1a69dff663af5e41922f5b7745260aaa74b95fe
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-10-16 16:38:54 -0500

    Revert "Merge branch 'main' of https://github.com/Valant09/coffee-front"
    This reverts commit 7eb5c65b5e88d9528683778165e7d67961d3d1ce, reversing
    changes made to 76ce4c774c76ce984edf630766c3f37cf051acf1.

--- coffee-front/src/app/app.module.ts
+++ coffee-front/src/app/app.module.ts
@@ -9,31 +9,20 @@ import { RegisterComponent } from './users/register/register.component';
 import { FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { ProductListComponent } from './products/product-list/product-list.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ProductListComponent,
-
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatGridListModule,
-    MatCardModule,
-    FlexLayoutModule,
-  
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
