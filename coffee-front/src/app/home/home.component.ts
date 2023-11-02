aa1b755b77a74a7bdaec4c99d1ddc0fd8bf15708
DanielBarreto01 <125820921+DanielBarreto01@users.noreply.github.com>
2023-11-02 17:33:41 -0500

    sliders
    falta ajustar el tamaño de las imágenes al tamaño de slider

--- coffee-front/src/app/home/home.component.ts
+++ coffee-front/src/app/home/home.component.ts
@@ -1,15 +1,30 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit,Renderer2  } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('input[name="slider"]');
+    const maxIndex = slides.length;
+
+    setInterval(() => {
+      if ('checked' in slides[currentIndex]) {
+        (slides[currentIndex] as HTMLInputElement).checked = false;
+        currentIndex = (currentIndex + 1) % maxIndex;
+        (slides[currentIndex] as HTMLInputElement).checked = true;
+      }
+    }, 3000); // Cambiar cada 3 segundos
+  }
 
   onSearch(searchTerm: string) {
     this.search.emit(searchTerm);
   }
-
 }
