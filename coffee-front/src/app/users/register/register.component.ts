a73fe56ac1b9f4e577d2c99687f4d8396dd346cd
Cristian Fernando Becerra Guerrero <cfbg99@hotmail.es>
2023-11-07 00:13:50 -0500

    Add SweetAlert2 library and implement user registration with form validation

--- coffee-front/src/app/users/register/register.component.ts
+++ coffee-front/src/app/users/register/register.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { UserServiceService } from '../../services/user-service.service';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register',
@@ -10,6 +13,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class RegisterComponent {
   formValid = false;
   res: any;
+  usuarioCreado: boolean = false;
 
   formularioPersona = new FormGroup({
     nombre_usuario: new FormControl('', [Validators.required, Validators.maxLength(30), Validators.pattern(/^[A-Za-z\s]+$/)]),
@@ -21,28 +25,57 @@ export class RegisterComponent {
     tipo_usuario: new FormControl('C', [Validators.required, Validators.maxLength(80)]),
   });
 
-  constructor(private userService: UserServiceService) {
+  constructor(private userService: UserServiceService,private router: Router) {
     this.formularioPersona.valueChanges.subscribe(() => {
       this.formValid = this.formularioPersona.valid;
     });
   }
 
+ 
+  
   addPersona() {
     if (this.formularioPersona.valid) {
       const formData = this.formularioPersona.value;
       console.log(formData);
 
-      this.userService.postRequest(formData)
-        .subscribe(res => {
-          this.res = res;
-          console.log(this.res);
-        },
-          error => {
-            console.error(error);
-            console.log('Error al Registrar usuario', 'Error de Registro');
-          });
+      this.enviarSolicitud(formData);
     } else {
       // El formulario no es válido, muestra un mensaje de error o realiza alguna otra acción.
+    }
+  }
+
+  enviarSolicitud(formData: any) {
+    this.userService.postRequest(formData)
+      .subscribe(res => {
+        this.res = res;
+        console.log("Res:", this.res);
+        
+        // Llama al método para mostrar la alerta y verifica la propiedad "message"
+        this.mostrarAlerta(this.res.message === "Usuario creado correctamente");
+      },
+      error => {
+        console.error(error);
+        console.log('Error al Registrar usuario', 'Error de Registro');
+      });
+  }
+  
+  mostrarAlerta(esExitoso: boolean) {
+    if (esExitoso) {
+      Swal.fire({
+        title: 'Registro Exitoso',
+        text: '¡El usuario se ha registrado con éxito!',
+        icon: 'success',
+        confirmButtonText: 'Aceptar'
+      }).then(() => {
+        // Redirige al usuario a la página de inicio de sesión
+        this.router.navigate(['/login']);
+      });
+    } else {
+      Swal.fire({
+        title: 'Error en el registro',
+        text: 'Hubo un problema al registrar al usuario.',
+        icon: 'error'
+      });
     }
   }
 }
